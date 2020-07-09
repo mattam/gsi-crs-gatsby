935cc0cbbb5f4872cd44735582554305e0d616d7
Matthew Chan <mattam@gmail.com>
2020-07-09 15:51:56 -0700

    Changing up the gatsby deployment so that it knows how to read in Strapi stuff

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
-// custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
-
-import "prismjs/themes/prism.css"
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+import "./src/css/index.css"
